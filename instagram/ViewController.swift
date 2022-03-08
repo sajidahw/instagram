f0982310c60b6cc440dedfa925c820305d4edceb
sajidahw <88634981+sajidahw@users.noreply.github.com>
2022-03-07 20:27:37 -0800

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Sajidah Wahdy on 3/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
